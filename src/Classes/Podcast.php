0406fd77809e9d5829ee19627e94969409d6511b
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-09-18 11:09:25 +0330

    move to src/Classes/

--- src/Classes/Podcast.php
+++ src/Classes/Podcast.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license MIT
  */
 
-namespace Respinar\ContaoPodcastBundle;
+namespace Respinar\PodcastBundle\Classes;
 
 use Contao\ContentModel;
 use Contao\System;
@@ -26,8 +26,8 @@ use Contao\Date;
 use Contao\File;
 use Contao\Model\Collection;
 use Contao\ModuleModel;
-use Respinar\ContaoPodcastBundle\Model\ChannelModel;
-use Respinar\ContaoPodcastBundle\Model\EpisodeModel;
+use Respinar\PodcastBundle\Model\ChannelModel;
+use Respinar\PodcastBundle\Model\EpisodeModel;
 
 class Podcast {
 
