f558d3e9c85a3e1965d9f3679d2a80eea6d59e98
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-11-25 22:42:48 +0330

    improve

--- src/Classes/PodcastParser.php
+++ src/Classes/PodcastParser.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace Respinar\PodcastBundle\Classes;
 
 use Contao\ContentModel;
+use Contao\CoreBundle\Image\Studio\Figure;
 use Contao\CoreBundle\Image\Studio\Studio;
 use Contao\CoreBundle\Routing\ContentUrlGenerator;
 use Contao\Date;
@@ -29,117 +30,124 @@ use Respinar\PodcastBundle\Model\EpisodeModel;
 
 final readonly class PodcastParser
 {
-	public function __construct(
-		private Studio $studio,
-		private PodcastSchema $schema,
-		private ContentUrlGenerator $contentUrlGenerator,
-	) {}
+    public function __construct(
+        private Studio $studio,
+        private PodcastSchema $schema,
+        private ContentUrlGenerator $contentUrlGenerator,
+        private PodcastUtil $podcastUtil,
+    ) {}
 
-	public function parseEpisode(EpisodeModel $episode, ModuleModel|ContentModel $model, PageModel $page,): string
-	{
-		$template = new FrontendTemplate($model->podcast_template);
+    public function parseEpisode(EpisodeModel $episode, ModuleModel|ContentModel $model, PageModel $page,): string
+    {
+        $template = new FrontendTemplate($model->podcast_template);
 
-		$template->setData($episode->row());
+        $template->setData($episode->row());
 
-		$template->link = $this->contentUrlGenerator->generate($episode);		
+        $template->link = $this->contentUrlGenerator->generate($episode);
 
-		$template->date = Date::parse($page->dateFormat, $episode->date);
+        $template->date = Date::parse($page->dateFormat, $episode->date);
 
-		$template->duration = PodcastUtil::getDuration((int) $episode->duration);
+        $template->duration = $this->podcastUtil->getDuration((int) $episode->duration);
 
-		if (($author = $episode->getRelated('author')) instanceof UserModel) {
-			$template->author = $GLOBALS['TL_LANG']['MSC']['by'] . ' ' . $author->name;
-			$template->authorModel = $author;
-		}
+        if (($author = $episode->getRelated('author')) instanceof UserModel) {
+            $template->author = $GLOBALS['TL_LANG']['MSC']['by'] . ' ' . $author->name;
+            $template->authorModel = $author;
+        }
 
-		$this->addFigure($template, $episode, $model);
+        $this->addFigure($template, $episode, $model);
 
-		$mediaUrl = $this->addAudio($template, $episode, $page);
+        $mediaUrl = $this->addAudio($template, $episode, $page);
 
-		$schema = $this->schema->generate($episode, $model, $mediaUrl);
+        $schema = $this->schema->generate($episode, $mediaUrl);
 
-		$template->schemaOrgData = $schema;
+        $template->schemaOrgData = $schema;
 
-		return $template->parse();
-	}
+        return $template->parse();
+    }
 
-	public function parseEpisodes(
-		Collection $episodes,
-		ModuleModel|ContentModel $model,
-		PageModel $page,
-	): array {
-		$items = [];
+    public function parseEpisodes(
+        Collection $episodes,
+        ModuleModel|ContentModel $model,
+        PageModel $page,
+    ): array {
+        $items = [];
 
-		foreach ($episodes as $episode) {
-			$items[] = $this->parseEpisode($episode, $model, $page,);
-		}
+        foreach ($episodes as $episode) {
+            $items[] = $this->parseEpisode($episode, $model, $page,);
+        }
 
-		return $items;
-	}
+        return $items;
+    }
 
-	private function addFigure(
-		FrontendTemplate $template,
-		EpisodeModel $episode,
-		ModuleModel|ContentModel $model,
-	): void {
-		if (!$episode->coverSRC) {
-			return;
-		}
+    private function addFigure(
+        FrontendTemplate $template,
+        EpisodeModel $episode,
+        ModuleModel|ContentModel $model,
+    ): void {
+        if (!$episode->coverSRC) {
+            return;
+        }
 
-		$size = null;
+        $size = null;
 
-		if ($model->imgSize) {
-			$imgSize = StringUtil::deserialize($model->imgSize);
+        if ($model->imgSize) {
+            $imgSize = StringUtil::deserialize($model->imgSize);
 
-			if (
-				$imgSize[0] > 0
-				|| $imgSize[1] > 0
-				|| is_numeric($imgSize[2])
-				|| ($imgSize[2][0] ?? null) === '_'
-			) {
-				$size = $model->imgSize;
-			}
-		}
+            if (
+                $imgSize[0] > 0
+                || $imgSize[1] > 0
+                || is_numeric($imgSize[2])
+                || ($imgSize[2][0] ?? null) === '_'
+            ) {
+                $size = $model->imgSize;
+            }
+        }
 
-		$figure = $this->studio
-			->createFigureBuilder()
-			->from($episode->coverSRC)
-			->setSize($size)
-			->buildIfResourceExists();
+        $figure = $this->studio
+            ->createFigureBuilder()
+            ->from($episode->coverSRC)
+            ->setSize($size)
+            ->buildIfResourceExists();
 
-		if ($figure instanceof Figure) {
-			$template->figure = $figure;
-		}
-	}
+        if ($figure instanceof Figure) {
+            $template->figure = $figure;
+        }
+    }
 
-	private function addAudio(FrontendTemplate $template, EpisodeModel $episode, PageModel $page,): ?string
-	{
-		if (!$episode->podcastSRC) {
-			return null;
-		}
+    private function addAudio(FrontendTemplate $template, EpisodeModel $episode, PageModel $page,): ?string
+    {
+        if (!$episode->podcastSRC) {
+            return null;
+        }
 
-		$fileModel = FilesModel::findByUuid($episode->podcastSRC);
+        $fileModel = FilesModel::findByUuid($episode->podcastSRC);
 
-		if (!$fileModel instanceof FilesModel) {
-			return null;
-		}
+        if (!$fileModel instanceof FilesModel) {
+            return null;
+        }
 
-		$locale = LocaleUtil::formatAsLocale($page->language ?: 'en');
+        $locale = LocaleUtil::formatAsLocale($page->language ?: 'en');
 
-		$meta = $fileModel->getMetadata($locale);
+        $meta = $fileModel->getMetadata($locale);
 
-		$file = new File($fileModel->path);
+        		$file = new File($fileModel->path);
 
-		$file->title = StringUtil::specialchars(
-			$meta?->getTitle() ?: $file->name
-		);
+        		$file->title = StringUtil::specialchars(
+        			$meta?->getTitle() ?: $file->name
+        		);
 
-		$template->file = $file;
+        		$template->file = [
+        			'name' => $file->name,
+        			'path' => $file->path,
+        			'mime' => $file->mime,
+        			'title' => $file->title,
+        			'extension' => $file->extension,
+        		];
 
-		if ($meta !== null) {
-			$template->caption = $meta->getCaption();
-		}
+        		if ($meta !== null) {
+        			$template->caption = $meta->getCaption();
+        		}
 
-		return '/' . $file->path;
-	}
+        		return '/' . $file->path;
+    }
 }
