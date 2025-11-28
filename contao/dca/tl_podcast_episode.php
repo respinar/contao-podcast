b4298c3e480c0ae9fbc0d5f9dd669ad766727eb0
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-11-28 23:38:05 +0330

    improved by ecs check

--- contao/dca/tl_podcast_episode.php
+++ contao/dca/tl_podcast_episode.php
@@ -11,77 +11,74 @@ declare(strict_types=1);
  */
 
 use Contao\Backend;
+use Contao\BackendUser;
 use Contao\DataContainer;
 use Contao\DC_Table;
-use Contao\Input;
-use Contao\BackendUser;
-use Contao\Config;
 use Contao\System;
-
 use Respinar\PodcastBundle\Model\ChannelModel;
 
 System::loadLanguageFile('tl_content');
 
-/**
+/*
  * Table tl_podcast_episode
  */
 $GLOBALS['TL_DCA']['tl_podcast_episode'] = [
-	'config' => [
-		'dataContainer' => DC_Table::class,
-		'ptable' => 'tl_podcast_channel',
-		'enableVersioning' => true,
-		'switchToEdit' => true,
-		'markAsCopy' => 'headline',
-		'sql' => [
-			'keys' => [
-				'id' => 'primary',
-				'alias' => 'index',
-				'pid,published,featured,start,stop' => 'index'
-			]
-		],
-	],
-	'list' => [
-		'sorting' => [
-			'mode' => DataContainer::MODE_PARENT,
-			'fields' => ['date'],
-			'flag' => DataContainer::SORT_INITIAL_LETTER_ASC,
-			'headerFields' => ['title', 'jumpTo', 'author', 'feed', 'feedAlias'],
+    'config' => [
+        'dataContainer' => DC_Table::class,
+        'ptable' => 'tl_podcast_channel',
+        'enableVersioning' => true,
+        'switchToEdit' => true,
+        'markAsCopy' => 'headline',
+        'sql' => [
+            'keys' => [
+                'id' => 'primary',
+                'alias' => 'index',
+                'pid,published,featured,start,stop' => 'index',
+            ],
+        ],
+    ],
+    'list' => [
+        'sorting' => [
+            'mode' => DataContainer::MODE_PARENT,
+            'fields' => ['date'],
+            'flag' => DataContainer::SORT_INITIAL_LETTER_ASC,
+            'headerFields' => ['title', 'jumpTo', 'author', 'feed', 'feedAlias'],
 
-			'panelLayout' => 'filter;sort,search,limit'
-		],
-		'label' => [
-			'fields' => ['date', 'episodeNumber', 'title'],
-			'format' => '<span class="label-info">[%s]</span> episode: %s - %s',
-		],
-		'global_operations' => [
-			'all' => [
-				'href' => 'act=select',
-				'class' => 'header_edit_all',
-				'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"'
-			]
-		],
-		'operations' => [
-			'edit',
-			'copy',
-			'delete',
-			'toggle' => [
-				'href' => 'act=toggle&amp;field=published',
-				'icon' => 'visible.svg',
-				'primary'      => true,
-				'showInHeader' => true
-			],
-			'feature' => [
-				'href' => 'act=toggle&amp;field=featured',
-				'icon' => 'featured.svg',
-				'primary'      => true,
-			],
-			'show',
-		]
-	],
+            'panelLayout' => 'filter;sort,search,limit',
+        ],
+        'label' => [
+            'fields' => ['date', 'episodeNumber', 'title'],
+            'format' => '<span class="label-info">[%s]</span> episode: %s - %s',
+        ],
+        'global_operations' => [
+            'all' => [
+                'href' => 'act=select',
+                'class' => 'header_edit_all',
+                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"',
+            ],
+        ],
+        'operations' => [
+            'edit',
+            'copy',
+            'delete',
+            'toggle' => [
+                'href' => 'act=toggle&amp;field=published',
+                'icon' => 'visible.svg',
+                'primary' => true,
+                'showInHeader' => true,
+            ],
+            'feature' => [
+                'href' => 'act=toggle&amp;field=featured',
+                'icon' => 'featured.svg',
+                'primary' => true,
+            ],
+            'show',
+        ],
+    ],
 
-	// Palettes
-	'palettes' => [
-		'default' => '
+    // Palettes
+    'palettes' => [
+        'default' => '
 			{title_legend},title,featured,alias,episodeNumber;
 			{date_legend},date,author;
 			{podcast_legend},podcastSRC;
@@ -89,175 +86,172 @@ $GLOBALS['TL_DCA']['tl_podcast_episode'] = [
 			{meta_legend},pageTitle,duration,description;
 			{teaser_legend},subheadline,teaser;
 			{expert_legend:hide},cssClass;
-			{publish_legend},published,start,stop'
-	],
+			{publish_legend},published,start,stop',
+    ],
 
-	// Fields	
-	'fields' => [
-		'id'  => [
-			'sql' => ['type' => 'integer', 'unsigned' => true, 'autoincrement' => true]
-		],
-		'pid' => [
-			'foreignKey' => 'tl_podcast_channel.title',
-			'sql' => ['type' => 'integer', 'unsigned' => true, 'default' => 0],
-			'relation' => ['type' => 'belongsTo', 'load' => 'lazy']
-		],
-		'tstamp'  => [
-			'sql' => ['type' => 'integer', 'unsigned' => true, 'default' => 0]
-		],
-		'title' => [
-			'search' => true,
-			'inputType' => 'text',
-			'flag' => DataContainer::SORT_INITIAL_LETTER_ASC,
-			'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
-			'sql' => "varchar(255) NOT NULL default ''"
-		],
-		'featured' => [
-			'toggle' => true,
-			'filter' => true,
-			'inputType' => 'checkbox',
-			'eval' => ['tl_class' => 'w50 m12'],
-			'sql' => ['type' => 'boolean', 'default' => false]
-		],
-		'alias' => [
-			'search' => true,
-			'inputType' => 'text',
-			'eval' => ['rgxp' => 'alias', 'doNotCopy' => true, 'unique' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
-			'save_callback' => [
-				['tl_podcast_episode', 'generateAlias']
-			],
-			'sql' => "varchar(255) BINARY NOT NULL default ''"
-		],
-		'author' => [
-			'default' => BackendUser::getInstance()->id,
-			'search' => true,
-			'filter' => true,
-			'sorting' => true,
-			'flag' => DataContainer::SORT_ASC,
-			'inputType' => 'select',
-			'foreignKey' => 'tl_user.name',
-			'eval' => ['doNotCopy' => true, 'chosen' => true, 'mandatory' => true, 'includeBlankOption' => true, 'tl_class' => 'w50'],
-			'sql' => "int(10) unsigned NOT NULL default 0",
-			'relation' => ['type' => 'hasOne', 'load' => 'lazy']
-		],
-		'date' => [
-			'default' => time(),
-			'filter' => true,
-			'sorting' => true,
-			'flag' => DataContainer::SORT_MONTH_DESC,
-			'inputType' => 'text',
-			'eval' => ['rgxp' => 'date', 'mandatory' => true, 'doNotCopy' => true, 'datepicker' => true, 'tl_class' => 'w50 wizard'],
-			'sql' => "int(10) unsigned NOT NULL default 0"
-		],
-		'episodeNumber' => [
-			'sorting' => true,
-			'inputType' => 'text',
-			'eval' => ['rgxp' => 'number', 'mandatory' => true, 'doNotCopy' => true, 'tl_class' => 'w50'],
-			'sql' => "int(10) unsigned NULL"
-		],
-		'pageTitle' => [
-			'search' => true,
-			'inputType' => 'text',
-			'eval' => ['maxlength' => 255, 'decodeEntities' => true, 'tl_class' => 'w50'],
-			'sql' => "varchar(255) NOT NULL default ''"
-		],
-		'description' => [
-			'search' => true,
-			'inputType' => 'textarea',
-			'eval' => ['style' => 'height:60px', 'decodeEntities' => true, 'tl_class' => 'clr'],
-			'sql' => "text NULL"
-		],
-		'duration' => [
-			'inputType' => 'text',
-			'eval' => ['rgxp' => 'number', 'mandatory' => true, 'doNotCopy' => true, 'tl_class' => 'w50'],
-			'sql' => "int(5) unsigned NULL"
-		],
-		'subheadline' => [
-			'search' => true,
-			'inputType' => 'text',
-			'eval' => ['maxlength' => 255, 'tl_class' => 'long'],
-			'sql' => "varchar(255) NOT NULL default ''"
-		],
-		'teaser' => [
-			'search' => true,
-			'inputType' => 'textarea',
-			'eval' => ['rte' => 'tinyMCE', 'tl_class' => 'clr'],
-			'sql' => "text NULL"
-		],
-		'coverSRC' => [
-			'inputType' => 'fileTree',
-			'eval' => ['fieldType' => 'radio', 'filesOnly' => true, 'extensions' => '%contao.image.valid_extensions%', 'mandatory' => true],
-			'sql' => "binary(16) NULL"
-		],
-		'podcastSRC' => [
-			'inputType' => 'fileTree',
-			'eval' => ['multiple' => false, 'fieldType' => 'radio', 'filesOnly' => true, 'isDownloads' => true, 'extensions' => 'mp3, m4a, ogg', 'mandatory' => true],
-			'sql' => "binary(16) NULL"
-		],
-		'cssClass' => [
-			'inputType' => 'text',
-			'eval' => ['tl_class' => 'w50'],
-			'sql' => "varchar(255) NOT NULL default ''"
-		],
+    // Fields
+    'fields' => [
+        'id' => [
+            'sql' => ['type' => 'integer', 'unsigned' => true, 'autoincrement' => true],
+        ],
+        'pid' => [
+            'foreignKey' => 'tl_podcast_channel.title',
+            'sql' => ['type' => 'integer', 'unsigned' => true, 'default' => 0],
+            'relation' => ['type' => 'belongsTo', 'load' => 'lazy'],
+        ],
+        'tstamp' => [
+            'sql' => ['type' => 'integer', 'unsigned' => true, 'default' => 0],
+        ],
+        'title' => [
+            'search' => true,
+            'inputType' => 'text',
+            'flag' => DataContainer::SORT_INITIAL_LETTER_ASC,
+            'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
+        'featured' => [
+            'toggle' => true,
+            'filter' => true,
+            'inputType' => 'checkbox',
+            'eval' => ['tl_class' => 'w50 m12'],
+            'sql' => ['type' => 'boolean', 'default' => false],
+        ],
+        'alias' => [
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['rgxp' => 'alias', 'doNotCopy' => true, 'unique' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+            'save_callback' => [
+                ['tl_podcast_episode', 'generateAlias'],
+            ],
+            'sql' => "varchar(255) BINARY NOT NULL default ''",
+        ],
+        'author' => [
+            'default' => BackendUser::getInstance()->id,
+            'search' => true,
+            'filter' => true,
+            'sorting' => true,
+            'flag' => DataContainer::SORT_ASC,
+            'inputType' => 'select',
+            'foreignKey' => 'tl_user.name',
+            'eval' => ['doNotCopy' => true, 'chosen' => true, 'mandatory' => true, 'includeBlankOption' => true, 'tl_class' => 'w50'],
+            'sql' => 'int(10) unsigned NOT NULL default 0',
+            'relation' => ['type' => 'hasOne', 'load' => 'lazy'],
+        ],
+        'date' => [
+            'default' => time(),
+            'filter' => true,
+            'sorting' => true,
+            'flag' => DataContainer::SORT_MONTH_DESC,
+            'inputType' => 'text',
+            'eval' => ['rgxp' => 'date', 'mandatory' => true, 'doNotCopy' => true, 'datepicker' => true, 'tl_class' => 'w50 wizard'],
+            'sql' => 'int(10) unsigned NOT NULL default 0',
+        ],
+        'episodeNumber' => [
+            'sorting' => true,
+            'inputType' => 'text',
+            'eval' => ['rgxp' => 'number', 'mandatory' => true, 'doNotCopy' => true, 'tl_class' => 'w50'],
+            'sql' => 'int(10) unsigned NULL',
+        ],
+        'pageTitle' => [
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['maxlength' => 255, 'decodeEntities' => true, 'tl_class' => 'w50'],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
+        'description' => [
+            'search' => true,
+            'inputType' => 'textarea',
+            'eval' => ['style' => 'height:60px', 'decodeEntities' => true, 'tl_class' => 'clr'],
+            'sql' => 'text NULL',
+        ],
+        'duration' => [
+            'inputType' => 'text',
+            'eval' => ['rgxp' => 'number', 'mandatory' => true, 'doNotCopy' => true, 'tl_class' => 'w50'],
+            'sql' => 'int(5) unsigned NULL',
+        ],
+        'subheadline' => [
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['maxlength' => 255, 'tl_class' => 'long'],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
+        'teaser' => [
+            'search' => true,
+            'inputType' => 'textarea',
+            'eval' => ['rte' => 'tinyMCE', 'tl_class' => 'clr'],
+            'sql' => 'text NULL',
+        ],
+        'coverSRC' => [
+            'inputType' => 'fileTree',
+            'eval' => ['fieldType' => 'radio', 'filesOnly' => true, 'extensions' => '%contao.image.valid_extensions%', 'mandatory' => true],
+            'sql' => 'binary(16) NULL',
+        ],
+        'podcastSRC' => [
+            'inputType' => 'fileTree',
+            'eval' => ['multiple' => false, 'fieldType' => 'radio', 'filesOnly' => true, 'isDownloads' => true, 'extensions' => 'mp3, m4a, ogg', 'mandatory' => true],
+            'sql' => 'binary(16) NULL',
+        ],
+        'cssClass' => [
+            'inputType' => 'text',
+            'eval' => ['tl_class' => 'w50'],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
 
-		'published' => [
-			'toggle' => true,
-			'filter' => true,
-			'flag' => DataContainer::SORT_INITIAL_LETTER_ASC,
-			'inputType' => 'checkbox',
-			'eval' => ['doNotCopy' => true],
-			'sql' => ['type' => 'boolean', 'default' => true]
-		],
-		'start' => [
-			'inputType' => 'text',
-			'eval' => ['rgxp' => 'datim', 'datepicker' => true, 'tl_class' => 'w50 wizard'],
-			'sql' => "varchar(10) NOT NULL default ''"
-		],
-		'stop' => [
-			'inputType' => 'text',
-			'eval' => ['rgxp' => 'datim', 'datepicker' => true, 'tl_class' => 'w50 wizard'],
-			'sql' => "varchar(10) NOT NULL default ''"
-		]
-	]
+        'published' => [
+            'toggle' => true,
+            'filter' => true,
+            'flag' => DataContainer::SORT_INITIAL_LETTER_ASC,
+            'inputType' => 'checkbox',
+            'eval' => ['doNotCopy' => true],
+            'sql' => ['type' => 'boolean', 'default' => true],
+        ],
+        'start' => [
+            'inputType' => 'text',
+            'eval' => ['rgxp' => 'datim', 'datepicker' => true, 'tl_class' => 'w50 wizard'],
+            'sql' => "varchar(10) NOT NULL default ''",
+        ],
+        'stop' => [
+            'inputType' => 'text',
+            'eval' => ['rgxp' => 'datim', 'datepicker' => true, 'tl_class' => 'w50 wizard'],
+            'sql' => "varchar(10) NOT NULL default ''",
+        ],
+    ],
 ];
 
-	/**
-	 * Provide miscellaneous methods that are used by the data configuration array.
-	 */
+/**
+ * Provide miscellaneous methods that are used by the data configuration array.
+ */
 class tl_podcast_episode extends Backend
 {
-	/**
-	 * Import the back end user object
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-		$this->import(BackendUser::class, 'User');
-	}
+    /**
+     * Import the back end user object.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->import(BackendUser::class, 'User');
+    }
 
-	/**
-     * Auto-generate the episode alias if it has not been set yet
-     *
-     * @param mixed         $varValue
+    /**
+     * Auto-generate the episode alias if it has not been set yet.
      *
      * @return string
+     *
      * @throws Exception
      */
     public function generateAlias($varValue, DataContainer $dc)
-	{
-		$aliasExists = function (string $alias) use ($dc): bool {
-			return $this->Database->prepare("SELECT id FROM tl_podcast_episode WHERE alias=? AND id!=?")->execute($alias, $dc->id)->numRows > 0;
-		};
+    {
+        $aliasExists = fn (string $alias): bool => $this->Database->prepare('SELECT id FROM tl_podcast_episode WHERE alias=? AND id!=?')->execute($alias, $dc->id)->numRows > 0;
 
-		// Generate alias if there is none
-		if (!$varValue) {
-			$varValue = System::getContainer()->get('contao.slug')->generate($dc->activeRecord->title, ChannelModel::findByPk($dc->activeRecord->pid)->jumpTo, $aliasExists);
-		} elseif (preg_match('/^[1-9]\d*$/', $varValue)) {
-			throw new Exception(sprintf($GLOBALS['TL_LANG']['ERR']['aliasNumeric'], $varValue));
-		} elseif ($aliasExists($varValue)) {
-			throw new Exception(sprintf($GLOBALS['TL_LANG']['ERR']['aliasExists'], $varValue));
-		}
+        // Generate alias if there is none
+        if (!$varValue) {
+            $varValue = System::getContainer()->get('contao.slug')->generate($dc->activeRecord->title, ChannelModel::findById($dc->activeRecord->pid)->jumpTo, $aliasExists);
+        } elseif (preg_match('/^[1-9]\d*$/', $varValue)) {
+            throw new Exception(sprintf($GLOBALS['TL_LANG']['ERR']['aliasNumeric'], $varValue));
+        } elseif ($aliasExists($varValue)) {
+            throw new Exception(sprintf($GLOBALS['TL_LANG']['ERR']['aliasExists'], $varValue));
+        }
 
-		return $varValue;
-	}
+        return $varValue;
+    }
 }
