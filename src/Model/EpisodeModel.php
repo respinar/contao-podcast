2eb3537179353d93034e134a03268b036e9cbd96
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-12-15 23:38:10 +0330

    improved by ecs check

--- src/Model/EpisodeModel.php
+++ src/Model/EpisodeModel.php
@@ -20,120 +20,105 @@ class EpisodeModel extends Model
 {
     protected static $strTable = 'tl_podcast_episode';
 
-	/**
-	 * Find a published episode from one or more podcast channel by its ID or alias
-	 */
-	public static function findPublishedByParentAndIdOrAlias($varId, $arrPids, array $arrOptions=array()): EpisodeModel|null
-	{
-		if (empty($arrPids) || !\is_array($arrPids))
-		{
-			return null;
-		}
+    /**
+     * Find a published episode from one or more podcast channel by its ID or alias.
+     */
+    public static function findPublishedByParentAndIdOrAlias($varId, $arrPids, array $arrOptions = []): self|null
+    {
+        if (empty($arrPids) || !\is_array($arrPids)) {
+            return null;
+        }
 
-		$t = static::$strTable;
-		$arrColumns = preg_match('/^[1-9]\d*$/', $varId) ? array("$t.id=?") : array("CAST($t.alias AS BINARY)=?");
-		$arrColumns[] = "$t.pid IN(" . implode(',', array_map('\intval', $arrPids)) . ")";
+        $t = static::$strTable;
+        $arrColumns = preg_match('/^[1-9]\d*$/', $varId) ? ["$t.id=?"] : ["CAST($t.alias AS BINARY)=?"];
+        $arrColumns[] = "$t.pid IN(".implode(',', array_map('\intval', $arrPids)).')';
 
-		if (!static::isPreviewMode($arrOptions))
-		{
-			$time = Date::floorToMinute();
-			$arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
-		}
+        if (!static::isPreviewMode($arrOptions)) {
+            $time = Date::floorToMinute();
+            $arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
+        }
 
-		return static::findOneBy($arrColumns, array($varId), $arrOptions);
-	}
+        return static::findOneBy($arrColumns, [$varId], $arrOptions);
+    }
 
-	/**
-	 * Find published episodes with the default redirect target by their parent ID	 
-	 */
-	public static function findPublishedDefaultByPid(int $intPid, array $arrOptions=array()): Collection|EpisodeModel|null
-	{
-		$t = static::$strTable;
-		$arrColumns = array("$t.pid=?");
+    /**
+     * Find published episodes with the default redirect target by their parent ID.
+     */
+    public static function findPublishedDefaultByPid(int $intPid, array $arrOptions = []): Collection|self|null
+    {
+        $t = static::$strTable;
+        $arrColumns = ["$t.pid=?"];
 
-		if (!static::isPreviewMode($arrOptions))
-		{
-			$time = Date::floorToMinute();
-			$arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
-		}
+        if (!static::isPreviewMode($arrOptions)) {
+            $time = Date::floorToMinute();
+            $arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
+        }
 
-		if (!isset($arrOptions['order']))
-		{
-			$arrOptions['order'] = "$t.date DESC";
-		}
+        if (!isset($arrOptions['order'])) {
+            $arrOptions['order'] = "$t.date DESC";
+        }
 
-		return static::findBy($arrColumns, array($intPid), $arrOptions);
-	}
+        return static::findBy($arrColumns, [$intPid], $arrOptions);
+    }
 
-	/**
-	 * Count published episodes by their parent ID
-	 */
-	public static function countPublishedByPid(int $pid, ?bool $blnFeatured = null, array $arrOptions = []): int
-	{
-		if ($pid === 0)
-		{
-			return 0;
-		}
+    /**
+     * Count published episodes by their parent ID.
+     */
+    public static function countPublishedByPid(int $pid, bool|null $blnFeatured = null, array $arrOptions = []): int
+    {
+        if (0 === $pid) {
+            return 0;
+        }
 
-		$t = static::$strTable;
-		$arrColumns = array("$t.pid = $pid");
+        $t = static::$strTable;
+        $arrColumns = ["$t.pid = $pid"];
 
-		if ($blnFeatured === true)
-		{
-			$arrColumns[] = "$t.featured=1";
-		}
-		elseif ($blnFeatured === false)
-		{
-			$arrColumns[] = "$t.featured=''";
-		}
+        if (true === $blnFeatured) {
+            $arrColumns[] = "$t.featured=1";
+        } elseif (false === $blnFeatured) {
+            $arrColumns[] = "$t.featured=''";
+        }
 
-		if (!static::isPreviewMode($arrOptions))
-		{
-			$time = Date::floorToMinute();
-			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
-		}
+        if (!static::isPreviewMode($arrOptions)) {
+            $time = Date::floorToMinute();
+            $arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+        }
 
-		return static::countBy($arrColumns, null, $arrOptions);
-	}
+        return static::countBy($arrColumns, null, $arrOptions);
+    }
 
-	/**
-	 * Find published episodes by their parent ID
-	 */
-	public static function findPublishedByPid(int $pid, ?bool $blnFeatured = null, int $intLimit = 0, int $intOffset = 0, array $arrOptions = []): Collection|EpisodeModel|null
-	{
-		if ($pid === 0)
-		{
-			return null;
-		}
+    /**
+     * Find published episodes by their parent ID.
+     */
+    public static function findPublishedByPid(int $pid, bool|null $blnFeatured = null, int $intLimit = 0, int $intOffset = 0, array $arrOptions = []): Collection|self|null
+    {
+        if (0 === $pid) {
+            return null;
+        }
 
-		$t = static::$strTable;
-		$arrColumns = array("$t.pid = $pid");
+        $t = static::$strTable;
+        $arrColumns = ["$t.pid = $pid"];
 
-		if ($blnFeatured === true)
-		{
-			$arrColumns[] = "$t.featured=1";
-		}
-		elseif ($blnFeatured === false)
-		{
-			$arrColumns[] = "$t.featured=''";
-		}
+        if (true === $blnFeatured) {
+            $arrColumns[] = "$t.featured=1";
+        } elseif (false === $blnFeatured) {
+            $arrColumns[] = "$t.featured=''";
+        }
 
-		// Never return unpublished elements in the back end, so they don't end up in the RSS feed
-		if (!static::isPreviewMode($arrOptions))
-		{
-			$time = Date::floorToMinute();
-			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
-		}
+        // Never return unpublished elements in the back end, so they don't end up in the
+        // RSS feed
+        if (!static::isPreviewMode($arrOptions)) {
+            $time = Date::floorToMinute();
+            $arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+        }
 
-		if (!isset($arrOptions['order']))
-		{
-			$arrOptions['order']  = "$t.date DESC";
-		}
+        if (!isset($arrOptions['order'])) {
+            $arrOptions['order'] = "$t.date DESC";
+        }
 
-		$arrOptions['limit']  = $intLimit;
-		$arrOptions['offset'] = $intOffset;
+        $arrOptions['limit'] = $intLimit;
+        $arrOptions['offset'] = $intOffset;
 
-		return static::findBy($arrColumns, null, $arrOptions);
-	}
-
+        return static::findBy($arrColumns, null, $arrOptions);
+    }
 }
