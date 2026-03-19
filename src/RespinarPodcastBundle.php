0454023bb9066892e2ab08a028009755902abf70
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-19 23:58:27 +0330

    format by ecs

--- src/RespinarPodcastBundle.php
+++ src/RespinarPodcastBundle.php
@@ -18,7 +18,7 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class RespinarPodcastBundle extends AbstractBundle
 {
-    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder,): void
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
     {
         $containerConfigurator->import('../config/services.yaml');
     }
